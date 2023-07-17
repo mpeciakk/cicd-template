556dd974611d980cb5409685734ff2b2d0247802
mpeciakk <mpeciakk@gmail.com>
2023-07-17 22:39:06 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import express from "express"
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello world! Changes are pulled automatically too!")
+    res.send("Hello world!")
 })
 
 app.listen(3000, () => {
